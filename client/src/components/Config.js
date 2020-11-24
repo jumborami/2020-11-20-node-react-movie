3e51d7e2ad951c06a128db66aff0cce048b32007
boramings <jumborami@gmail.com>
2020-11-24 17:57:22 +0900

    commit

---
+++ client/src/components/Config.js
@@ -0,0 +1,7 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = '0d32823bbea4b0889cdd7c4bce3b1dbc';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+
---
+++ client/src/components/views/MovieDetail/Sections/Favorite.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from 'react'
+import Axios from 'axios'
+import { Button } from 'antd'
+
+function Favorite(props) {
+
+	const movieId = props.movieId
+	const userFrom = props.userFrom
+	const movieTitle = props.movieInfo.movieTitle
+	const moviePost = props.movieInfo.backdrop_path
+	const movieRunTime = props.movieInfo.runtime
+
+	const [FavoriteNumber, setFavoriteNumber] = useState(0)
+	const [Favorited, setFavorited] = useState(false)
+
+	let variables = {
+		userFrom: userFrom,
+		movieId: movieId,
+		movieTitle: movieTitle,
+		moviePost: moviePost,
+		movieRunTime: movieRunTime
+	}
+
+	useEffect(() => {
+		
+		Axios.post('/api/favorite/favoriteNumber', variables) //axios 리퀘스트
+			.then(response => {
+				console.log(response.data)
+				setFavoriteNumber(response.data.favoriteNumber)
+				if(response.data.success) {
+				} else {
+					alert('숫자 정보를 가져오는 데 실패했습니다.')
+				}
+			})
+		
+			Axios.post('/api/favorite/favorited', variables) //axios 리퀘스트
+			.then(response => {
+				console.log('favoirted', response.data)
+				if(response.data.success) {
+					setFavorited(response.data.favorited)					
+				} else {
+					alert('정보를 가져오는 데 실패했습니다.')
+				}
+			})
+	}, [])
+
+	const onClickFavorite = () => {
+		if(Favorited) {
+			Axios.post('/api/favorite/removeFromFavorite', variables)
+				.then(response => {
+					if(response.data.success) {
+						setFavoriteNumber(FavoriteNumber - 1)
+						setFavorited(!Favorited)
+					} else {
+							alert('Favorite 리스트에서 지우는 걸 실패했습니다.')
+					}
+				})
+		} else {
+			Axios.post('/api/favorite/addToFavorite', variables)
+				.then(response => {
+					if(response.data.success) {
+						setFavoriteNumber(FavoriteNumber + 1)
+						setFavorited(!Favorited)
+					} else {
+							alert('Favorite 리스트에서 추가하는 걸 실패했습니다.')
+					}
+				})
+		}
+	}
+
+	return (
+		<div>
+			<Button onClick={onClickFavorite}>{Favorited ? "Not Favorite" : "Add to Favorite"} {FavoriteNumber} </Button>
+		</div>
+	)
+}
+
+export default Favorite
